0c810ff3a768c370c2c4ac4b1d3d2fc2046cb6bc
Igor Martins <igorjosemartins01@gmail.com>
2026-03-11 00:23:59 -0300

    update env variables

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,13 +1,13 @@
 declare namespace NodeJS {
   interface Env {
     PORT: number;
-    BLIP_ROUTER_COMMAND_URL: string;
-    BLIP_ROUTER_AUTH_KEY: string;
+    BLIP_ROUTER_COMMANDS_URL: string;
+    BLIP_ROUTER_HTTP_KEY: string;
   }
 }
 
 export interface Env {
     PORT: number;
-    BLIP_ROUTER_COMMAND_URL: string;
-    BLIP_ROUTER_AUTH_KEY: string;
+    BLIP_ROUTER_COMMANDS_URL: string;
+    BLIP_ROUTER_HTTP_KEY: string;
 }
