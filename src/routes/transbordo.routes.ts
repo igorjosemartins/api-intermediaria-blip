07d8147548d75a80f09ce3d8067dabff8381a6c9
Igor Martins <igorjosemartins01@gmail.com>
2026-03-12 18:31:52 -0300

    new endpoint: delete /transbordo/all

--- src/routes/transbordo.routes.ts
+++ src/routes/transbordo.routes.ts
@@ -1,17 +1,18 @@
 import { FastifyInstance } from "fastify";
-import { migrateAttendants, migrateReplies, migrateTags, migrateTransbordo } from "../controllers/transbordo.controller";
+import { deleteTransbordo, migrateAttendants, migrateReplies, migrateTags, migrateTransbordo } from "../controllers/transbordo.controller";
 
 export default async function transbordoRoutes(app: FastifyInstance) {
   app.register(migrationRoutes, { prefix: "/migrate" });
+  app.register(deleteRoutes);
 };
 
 async function migrationRoutes(app: FastifyInstance) {
-  app.post("/transbordo", migrateTransbordo);
+  app.post("/all", migrateTransbordo);
   app.post("/attendants", migrateAttendants);
   app.post("/tags", migrateTags);
   app.post("/replies", migrateReplies);
 }
 
 async function deleteRoutes(app: FastifyInstance) {
-  // app.delete("/transbordo");
+  app.delete("/all", deleteTransbordo);
 }
