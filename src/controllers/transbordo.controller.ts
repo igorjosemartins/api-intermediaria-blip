181102c0e5f5a1526fa58f4200083618d65d0764
Igor Martins <igorjosemartins01@gmail.com>
2026-03-12 13:15:51 -0300

    new endpoint to only migrate tags: /migrate/tags

--- src/controllers/transbordo.controller.ts
+++ src/controllers/transbordo.controller.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { migrateTransbordoSchema } from "../schemas/transbordo.schema";
-import { attendantsMigration, transbordoMigration } from "../services/transbordo.service";
+import { attendantsMigration, getMissingTags, transbordoMigration } from "../services/transbordo.service";
 
 export const migrateTransbordo = async (req: FastifyRequest, reply: FastifyReply) => {
   const zodResult = migrateTransbordoSchema.safeParse(req.body);
@@ -33,6 +33,25 @@ export const migrateAttendants = async (req: FastifyRequest, reply: FastifyReply
 
   try {
     const results = await attendantsMigration(tenantId, origin, destiny);
+    
+    return reply.status(200).send(results);
+
+  } catch (e) {
+    return reply.status(500).send({ error: (e as Error).message });
+  }
+};
+
+export const migrateTags = async (req: FastifyRequest, reply: FastifyReply) => {
+  const zodResult = migrateTransbordoSchema.safeParse(req.body);
+
+  if (!zodResult.success) {
+    return reply.status(400).send({ error: z.prettifyError(zodResult.error) });
+  }
+
+  const { tenantId, origin, destiny } = zodResult.data;
+
+  try {
+    const results = await getMissingTags(tenantId, origin, destiny);
     
     return reply.status(200).send(results);
 
--- src/routes/transbordo.routes.ts
+++ src/routes/transbordo.routes.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from "fastify";
-import { migrateAttendants, migrateTransbordo } from "../controllers/transbordo.controller";
+import { migrateAttendants, migrateTags, migrateTransbordo } from "../controllers/transbordo.controller";
 
 export default async function transbordoRoutes (app: FastifyInstance) {
   app.post("/migrate", migrateTransbordo);
   app.post("/migrate/attendants", migrateAttendants);
+  app.post("/migrate/tags", migrateTags);
 };
